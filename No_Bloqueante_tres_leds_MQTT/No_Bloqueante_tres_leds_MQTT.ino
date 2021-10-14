9e8deb734f701e44b5d3aba49d6b500d0d6107af
jorgejp29 <87671602+jorgejp29@users.noreply.github.com>
2021-10-14 08:54:58 -0500

    No Bloqueante con MQTT y node-red
    En esta modificación, se incluye el envío de datos a node-red, utilizando MQTT y un Broker.

--- No_Bloqueante_tres_leds_MQTT/No_Bloqueante_tres_leds_MQTT.ino
+++ No_Bloqueante_tres_leds_MQTT/No_Bloqueante_tres_leds_MQTT.ino
@@ -20,7 +20,61 @@
 //
 //*****************************************************************************************
 
-#define CAMERA_MODEL_AI_THINKER
+#define CAMERA_MODEL_AI_THINKER // Has PSRAM
+
+#include <HCSR04.h>
+
+//UltraSonicDistanceSensor distanceSensor(12, 13);  // Initialize sensor that uses digital pins 13 and 12
+
+float distancia = 0;
+
+
+unsigned long t_0 = 0;
+unsigned long t_1 = 0;
+byte  corr = 0;
+
+/*
+ * Conexión básica por MQTT del NodeMCU
+ * por: Hugo Escalpelo
+ * Fecha: 28 de julio de 2021
+ * 
+ * Este programa envía datos  por Internet a través del protocolo MQTT. Para poder
+ * comprobar el funcionamiento de este programa, es necesario conectarse a un broker
+ * y usar NodeRed para visualzar que la información se está recibiendo correctamente.
+ * Este programa no requiere componentes adicionales.
+ * 
+ * Componente     PinESP32CAM     Estados lógicos
+ * ledStatus------GPIO 33---------On=>LOW, Off=>HIGH
+ * ledFlash-------GPIO 4----------On=>HIGH, Off=>LOW
+ */
+
+//Bibliotecas
+#include <WiFi.h>  // Biblioteca para el control de WiFi
+#include <PubSubClient.h> //Biblioteca para conexion MQTT
+
+//Datos de WiFi
+const char* ssid = "IZZI-319C";  // Aquí debes poner el nombre de tu red
+const char* password = "2WC456402918";  // Aquí debes poner la contraseña de tu red
+
+//Datos del broker MQTT
+//const char* mqtt_server = "3.122.36.163"; // Si estas en una red local, coloca la IP asignada, en caso contrario, coloca la IP publica
+const char* mqtt_server = "18.194.65.151"; 
+IPAddress server(3,122,36,163);
+
+// Objetos
+WiFiClient espClient; // Este objeto maneja los datos de conexion WiFi
+PubSubClient client(espClient); // Este objeto maneja los datos de conexion al broker
+
+// Variables
+int flashLedPin = 4;  // Para indicar el estatus de conexión
+int statusLedPin = 33; // Para ser controlado por MQTT
+long timeNow, timeLast; // Variables de control de tiempo no bloqueante
+int data = 0; // Contador
+int wait = 5000;  // Indica la espera cada 5 segundos para envío de mensajes MQTT
+
+//***************************************************************************
+//   Variables utilizadas en la secuenca y encendido apagado de e leds
+
 #define Led12 12
 #define Led13 13
 #define Led14 14
@@ -45,9 +99,64 @@ byte loop_Led2 = 0 ;
 byte loop_Led3 = 0 ;
 byte veces = 1;
 
+//***************************************************************************
 
+
+
+
+// Inicialización del programa
 void setup() {
-  //Configurando los LedsXX como salidas
+  // Iniciar comunicación serial
+  Serial.begin (115200);
+  pinMode (flashLedPin, OUTPUT);
+  pinMode (statusLedPin, OUTPUT);
+  digitalWrite (flashLedPin, LOW);
+  digitalWrite (statusLedPin, HIGH);
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Conectar a ");
+  Serial.println(ssid);
+ 
+  WiFi.begin(ssid, password); // Esta es la función que realiz la conexión a WiFi
+ 
+  while (WiFi.status() != WL_CONNECTED) { // Este bucle espera a que se realice la conexión
+    digitalWrite (statusLedPin, HIGH);
+    delay(500); //dado que es de suma importancia esperar a la conexión, debe usarse espera bloqueante
+    digitalWrite (statusLedPin, LOW);
+    Serial.print(".");  // Indicador de progreso
+    delay (5);
+  }
+  
+  // Cuando se haya logrado la conexión, el programa avanzará, por lo tanto, puede informarse lo siguiente
+  Serial.println();
+  Serial.println("WiFi conectado");
+  Serial.println("Direccion IP: ");
+  Serial.println(WiFi.localIP());
+
+  // Si se logro la conexión, encender led
+  if (WiFi.status () > 0){
+  digitalWrite (statusLedPin, LOW);
+  }
+  
+  delay (1000); // Esta espera es solo una formalidad antes de iniciar la comunicación con el broker
+
+  //Serial.end();
+
+  // Conexión con el broker MQTT
+  client.setServer(server, 1883); // Conectarse a la IP del broker en el puerto indicado
+  client.setCallback(callback); // Activar función de CallBack, permite recibir mensajes MQTT y ejecutar funciones a partir de ellos
+  //delay(1500);  // Esta espera es preventiva, espera a la conexión para no perder información
+  delay(1500);  // Esta espera es preventiva, espera a la conexión para no perder información
+
+  timeLast = millis (); // Inicia el control de tiempo
+
+  t_0 = millis();
+
+//**************************************************************************************
+//   Inicialización de variables utilizadas en la secuenca y encendido apagado de e leds
+
+    //Configurando los LedsXX como salidas
   pinMode(Led12,OUTPUT);
   pinMode(Led13,OUTPUT);
   pinMode(Led14,OUTPUT);
@@ -62,33 +171,130 @@ void setup() {
   t0_13 = t0_12;
   t0_14 = t0_12;
 
-   Serial.begin(9600);
+//**************************************************************************************
   
-}
+}// fin del void setup ()
 
+
+//*****************************************
+// Cuerpo del programa, bucle principal
+//*****************************************
 
 void loop() {
+
+  //distancia = distanceSensor.measureDistanceCm();
+
+  
+  //Verificar siempre que haya conexión al broker
+  if (!client.connected()) {
+    reconnect();  // En caso de que no haya conexión, ejecutar la función de reconexión, definida despues del void setup ()
+  }// fin del if (!client.connected())
+  client.loop(); // Esta función es muy importante, ejecuta de manera no bloqueante las funciones necesarias para la comunicación con el broker
+  
+  timeNow = millis(); // Control de tiempo para esperas no bloqueantes
+  if (timeNow - timeLast > wait) { // Manda un mensaje por MQTT cada cinco segundos
+    timeLast = timeNow; // Actualización de seguimiento de tiempo
+
+
+
+    char dataString[8]; // Define una arreglo de caracteres para enviarlos por MQTT, especifica la longitud del mensaje en 8 caracteres
+
   if(veces >=1  and veces <=8){
     if(veces == 1){
-      Serial.println("4 veces");
+      //Serial.println("4 veces");
+      data = 4;
+   
     }
         blink_Led12(2500);    //Blink de Led12 con intervalo de 5 seg de encndido y 5 seg apagado
   }
 
   if(veces >=9  and veces <= 24){
     if(veces == 9){
-      Serial.println("8 veces");
+      //Serial.println("8 veces");
+      data = 8;
+    
     }
         blink_Led13(1500);    //Blink de Led13 con intervalo de 3 seg de encndido y 3 seg apagado
   }
 
   if(veces >= 25 and veces <= 44){
     if(veces == 25){
-      Serial.println("10 veces");
+      //Serial.println("10 veces");
+      data = 10;
+     
     }
         blink_Led14(500);     //Blink de Led14 con intervalo de 0.5 seg de encndido y 0.5 seg apagado
   }
-}
+
+
+    
+    //data = distancia;
+    //dtostrf(data, 1, 2, dataString);
+    //client.publish("codigoiot/secuencia/jjaimes", dataString ); // Esta es la función que envía los datos por MQTT, especifica el tema y el valor
+
+
+
+    
+  }// fin del if (timeNow - timeLast > wait)
+}// fin del void loop ()
+
+// Funciones de usuario
+
+// Esta función permite tomar acciones en caso de que se reciba un mensaje correspondiente a un tema al cual se hará una suscripción
+void callback(char* topic, byte* message, unsigned int length) {
+
+  // Indicar por serial que llegó un mensaje
+  Serial.print("Llegó un mensaje en el tema: ");
+  Serial.print(topic);
+
+  // Concatenar los mensajes recibidos para conformarlos como una varialbe String
+  String messageTemp; // Se declara la variable en la cual se generará el mensaje completo  
+  for (int i = 0; i < length; i++) {  // Se imprime y concatena el mensaje
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+
+  // Se comprueba que el mensaje se haya concatenado correctamente
+  Serial.println();
+  Serial.print ("Mensaje concatenado en una sola variable: ");
+  Serial.println (messageTemp);
+
+  // En esta parte puedes agregar las funciones que requieras para actuar segun lo necesites al recibir un mensaje MQTT
+
+  // Ejemplo, en caso de recibir el mensaje true - false, se cambiará el estado del led soldado en la placa.
+  // El ESP323CAM está suscrito al tema esp/output
+  if (String(topic) == "codigoiot/foco/jjaimes") {  // En caso de recibirse mensaje en el tema esp32/output
+    if(messageTemp == "true"){
+      Serial.println("Led encendido");
+      digitalWrite(flashLedPin, HIGH);
+    }// fin del if (String(topic) == "codigoiot/foco/jjaimes")
+    else if(messageTemp == "false"){
+      Serial.println("Led apagado");
+      digitalWrite(flashLedPin, LOW);
+    }// fin del else if(messageTemp == "false")
+  }// fin del if (String(topic) == "codigoiot/foco/jjaimes")
+}// fin del void callback
+
+// Función para reconectarse
+void reconnect() {
+  // Bucle hasta lograr conexión
+  while (!client.connected()) { // Pregunta si hay conexión
+    Serial.print("Tratando de contectarse...");
+    // Intentar reconexión
+    if (client.connect("ESP32CAMClient")) { //Pregunta por el resultado del intento de conexión
+      Serial.println("Conectado");
+      client.subscribe("codigoiot/foco/jjaimes"); // Esta función realiza la suscripción al tema
+    }// fin del  if (client.connect("ESP32CAMClient"))
+    else {  //en caso de que la conexión no se logre
+      Serial.print("Conexion fallida, Error rc=");
+      Serial.print(client.state()); // Muestra el codigo de error
+      Serial.println(" Volviendo a intentar en 5 segundos");
+      // Espera de 5 segundos bloqueante
+      delay(5000);
+      Serial.println (client.connected ()); // Muestra estatus de conexión
+    }// fin del else
+  }// fin del bucle while (!client.connected())
+}// fin de void reconnect()
 
 
 
@@ -102,8 +308,12 @@ void blink_Led12(int tiempo){
     //Determinando el tiempó transcurrido
      if (t1_12 - t0_12 >= tiempo){              //Ya son 5 segundos...?
         t0_12 = millis();
+        char dataString[8];
         Led1 = !Led1;
         digitalWrite(Led12,Led1);  
+        data = 4;
+        dtostrf(data, 1, 2, dataString);
+        client.publish("codigoiot/secuencia/jjaimes", dataString );
          
         veces = veces + 1;   
      }
@@ -115,8 +325,12 @@ void blink_Led13(int tiempo){
     //Determinando el tiempó transcurrido
      if (t1_13 = t1_13 - t0_13 >= tiempo){              //Ya son 3 segundos...?
         t0_13 = millis();
+        char dataString[8];
         Led2 = !Led2;
         digitalWrite(Led13,Led2);
+        data = 8;
+        dtostrf(data, 1, 2, dataString);
+        client.publish("codigoiot/secuencia/jjaimes", dataString );
         
         veces = veces + 1;   
      }
@@ -127,8 +341,12 @@ void blink_Led14(int tiempo){
      //Determinando el tiempó transcurrido
      if (t1_14 - t0_14 >= tiempo){               //Ya son 500 mili segundos...?  
         t0_14 = millis();  
+        char dataString[8];
         Led3 = !Led3;
         digitalWrite(Led14,Led3);
+        data = 10;
+        dtostrf(data, 1, 2, dataString);
+        client.publish("codigoiot/secuencia/jjaimes", dataString );
         
        veces = veces + 1;   
         if(veces >=45){
